897c9702775bc9eb8f028f6c7f44f2508d5eddcd
慧 <kuramitsukeiichi@gmail.com>
2025-07-17 14:34:37 +0900

    2025_07_17

--- realtime_transform_body_2025_06_17/Assets/scripts/body_transform.cs
+++ realtime_transform_body_2025_06_17/Assets/scripts/body_transform.cs
@@ -15,9 +15,12 @@ public class body_transform : MonoBehaviour
     public HumanPose pose;
 
     public GameObject model;
-
-
     public Animator animator1;
+
+    public GameObject sholderBoneR;
+    public GameObject sholderBoneL;
+
+    Vector3 sholder2ElbowR;
 
 
     private void Start()
@@ -44,16 +47,24 @@ public class body_transform : MonoBehaviour
         poseHandler = new HumanPoseHandler(animator1.avatar, animator1.transform);
         poseHandler.GetHumanPose(ref pose);
         //pose.muscles[42] = -1.0f;
+
+        sholderBoneR
         
     }
 
     public void Update()
     {
-        Vector3 rotation = GetBoneRotation(Variable_Share.landmarks[2], Variable_Share.landmarks[3]);
-        Debug.Log(rotation.y);
+        Vector3 rotation = GetBoneRotation(Variable_Share.landmarks[2], Variable_Share.landmarks[4]);
+        Debug.Log( (180.0f - Vector3.Angle(Variable_Share.landmarks[4] - Variable_Share.landmarks[2], Variable_Share.landmarks[2] - Variable_Share.landmarks[0])) / 90.0f - 1.0f);
 
-        pose.muscles[42] = rotation.x;
-        //pose.muscles[42] = -1.0f;
+        //左腕の伸縮
+        pose.muscles[51] = (180.0f - Vector3.Angle(Variable_Share.landmarks[5] - Variable_Share.landmarks[3], Variable_Share.landmarks[3] - Variable_Share.landmarks[1])) / 90.0f - 1.0f;
+        
+        //右腕の伸縮
+        pose.muscles[42] = (180.0f - Vector3.Angle(Variable_Share.landmarks[4] - Variable_Share.landmarks[2], Variable_Share.landmarks[2] - Variable_Share.landmarks[0])) / 90.0f - 1.0f;
+
+        sholder2ElbowR = (Variable_Share.landmarks[4] - Variable_Share.landmarks[2]);
+        //pose.muscles[42] = rotation.x;
         poseHandler.SetHumanPose(ref pose);
 
     }
